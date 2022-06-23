cff52063414087f72c43225a0fa3766b52d71be2
lulullamozagamboa <luciallamozagamboa@gmail.com>
2022-06-23 11:12:26 -0300

    finaldeproyecto

---
+++ ejemReac.js
@@ -0,0 +1,12 @@
+console.log("hello world")
+var input = document.getElementsByClassName("formulario__input");
+for (var i=0; i < inputs.length; i++) {
+    inputs[1].addEventListener("keyup",funciona()){
+        if(this.IDBCursorWithValue.length>1)
+        this.nextElementSibling.classList.add("fijar");
+    } else {
+        this.nextElementSibling.classList.remove("fijar");
+    }
+
+    }
+}
--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-console.log("hello world")
-var input = document.getElementsByClassName("formulario__input");
-for (var i=0; i < inputs.length; i++) {
-    inputs[1].addEventListener("keyup",funciona()){
-        if(this.IDBCursorWithValue.length>1)
-        this.nextElementSibling.classList.add("fijar");
-    } else {
-        this.nextElementSibling.classList.remove("fijar");
-    }
-
-    }
-}
+var inputs = document.getElementsByClassName("formulario__input");
+ for (var i = 0; i < inputs.length; i++) {
+    inputs[i].addEventListener("keyup", funcion(){
+        if (this.value.length>=1) {
+            this.nextElementSibling.classList.add("fijar");
+            }else{ 
+            this.nextElementSibling.classList.add("fijar");  
+        }
+    });
+ }
